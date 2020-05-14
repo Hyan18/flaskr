8bf608ac7cc1bcafeb923543b49967115d78e006
Hyan18 <HarrisonYan18@gmail.com>
2020-05-14 15:17:54 +0100

    feat: user can register

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,9 @@ import os
 
 from flask import Flask
 
+from . import db
+from . import auth
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -32,10 +35,8 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-    from . import db
     db.init_app(app)
 
-    from . import auth
     app.register_blueprint(auth.bp)
 
     return app

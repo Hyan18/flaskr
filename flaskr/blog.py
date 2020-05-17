eeb559f188c7cf8286244e92605e899e5f4ae09b
Hyan18 <HarrisonYan18@gmail.com>
2020-05-17 14:48:41 +0100

    fix: login and update post

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -47,7 +47,8 @@ def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?'
+        ' WHERE p.id = ?',
+        (id,)
     ).fetchone()
 
     if post is None:
@@ -84,3 +85,12 @@ def update(id):
             return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM  post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
